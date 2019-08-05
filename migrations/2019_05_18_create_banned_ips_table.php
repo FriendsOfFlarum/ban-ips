f92a4d4a9bf26993e907bfdd6fc9af9839c3dd0b
David Sevilla Martin <dsevilla192@icloud.com>
2019-08-05 15:28:50 -0400

    Attempt to solve issue with migration by setting 'address' length to 191

--- migrations/2019_05_18_create_banned_ips_table.php
+++ migrations/2019_05_18_create_banned_ips_table.php
@@ -11,11 +11,12 @@
 
 use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
 return Migration::createTable('banned_ips', function (Blueprint $table) {
     $table->increments('id');
     $table->unsignedInteger('creator_id');
-    $table->string('address')->unique();
+    $table->string('address', 191)->unique();
     $table->string('reason')->nullable();
     $table->unsignedInteger('user_id')->nullable();
     $table->timestamp('created_at');
